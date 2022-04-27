3e419963ee687927966b4a47e529a0be6cb6ecf9
GuilhermeRafaelGomes <guilhermerafael2208@gmail.com>
2022-04-27 19:59:23 -0300

    adicionando classes do cardápio

---
+++ ProjetoHamburgueria/cardapio.py
@@ -0,0 +1,7 @@
+def mostrarMenu():
+    print('Escolha uma das opções abaixo\n' +
+          '\n1. Exercício    ' +
+          '\n2. Exercício    ' +
+          '\n3. Exercício    ' +
+          '\   ')
+    this.opcao = int(input())  # Coletar a digitação do usuário
