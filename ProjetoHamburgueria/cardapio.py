5dbb5d4dd7367c95e26cc98d8d40b5c2bb9a48fb
Vinícius <vinimice42@gmail.com>
2022-04-27 20:15:36 -0300

    Reiniciando projeto

--- ProjetoHamburgueria/cardapio.py
+++
@@ -1,7 +0,0 @@
-def mostrarMenu():
-    print('Escolha uma das opções abaixo\n' +
-          '\n1. Exercício    ' +
-          '\n2. Exercício    ' +
-          '\n3. Exercício    ' +
-          '\   ')
-    this.opcao = int(input())  # Coletar a digitação do usuário
