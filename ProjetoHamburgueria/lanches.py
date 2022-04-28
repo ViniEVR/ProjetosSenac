0890012b99f742366e580b6b7ee54ff330dffce9
GuilhermeRafaelGomes <guilhermerafael2208@gmail.com>
2022-04-27 21:33:22 -0300

    Adicionando cardápio dos lanches

---
+++ ProjetoHamburgueria/lanches.py
@@ -0,0 +1,17 @@
+def cardapioLanches():
+    print('Escolha o Hamburguer que deseja!\n' +
+          '\n\n\n1. Holly Fantástico R$ 39,90\n ingredientes: Pão brioche + burger 180g + rúcula + creme de gorgonzola + bacon bits + maionese defumada.  ' +
+          '\n\n\n2. Pulp Ficton Vincent R$ 26,90\n ingredientes: Pão, burger de 150g, american cheese, ketchup, mostarda, cebola branca e picles.   ' +
+          '\n\n\n3. Pulp Fiction Jules R$ 29,90\n ingredientes: Pão brioche, burger de 150g, cheddar e cebola caramelizada - acompanha maionese clássica.  ' +
+          '\n\n\n4. Holly Smoked Jam R$ 36,90\n ingredientes: Pão brioche, burger de 180g, queijo Gouda, maionese defumada, geléia de bacon apimentada.    ' +
+          '\n\n\n5. Holly Salad R$ 28,90\n ingredientes: Pão brioche + burger de 150g + american cheese + alface + tomate + cebola roxa - acompanha maionese verde.  ' +
+          '\n\n\n6. Holly Cheese R$ 24,90\n ingredientes: Pão brioche + burger 150g + american cheese - acompanha maionese verde.   ' +
+          '\n\n\n7. Holly Bacon R$ 29,90\n ingredientes: Pão brioche + burger 150g + american cheese + bacon caramelizado.   ' +
+          '\n\n\n8. Hulk R$ 29,90\n ingredientes: Pão brioche, double smashburger de 150g, cheddar, rúcula, cebola caramelizada e picles.   ' +
+          '\n\n\n9. King Kong R$ 47,90\n ingredientes: Pão, 2 burgers de 180g, american cheese, alface, tomate, cebola roxa, bacon e acompanha maionese de tabasco.    ' +
+          '\n\n\n10. Kill Bill R$ 39,90\n ingredientes: Pão brioche + burger 180g + cheddar cremoso e bacon bits, o cheddar e o bacon vão separados pra vc colocar como desejar.   ')
+    this.opcao = int(input())  # Coletar a digitação do usuário
+
+def coletar():
+    print(' ')
+    this. = float(input())
