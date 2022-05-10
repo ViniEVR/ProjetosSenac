e7d7d8b168012a95bc02ee7ef18a034fa8c5b0f0
GuilhermeRafaelGomes <guilhermerafael2208@gmail.com>
2022-05-09 22:10:38 -0300

    Finalizando projeto em aula

--- ProjetoHamburgueria/sobremesas.py
+++ ProjetoHamburgueria/sobremesas.py
@@ -4,52 +4,19 @@ import cardapio
 import operacoes
 
 this.opcaoSobremesas = 0
-def cardapioSobremesa():
+
+def cardapioSobremesas():
     operacoes.selecionarSobremesa()
+    print('-1 Voltar ao cardápio\n')
+
     this.opcaoSobremesas = int(input('Opção selecionada: '))  # Coletando a sobremesa selecionado
 
 def selecionarSobremesas():
     try:
-        while this.opcaoSobremesas != 7:
-            cardapioSobremesa()
-            if this.opcaoSobremesas == 1:
-                print('____________________________________________')
-                print('\nVocê adicionou Torta Gelada Negresco ao seu pedido\n')
-                print('____________________________________________')
-                conta.calculoValor(64.90, 'Torta gelada Negresco')
-
-            elif this.opcaoSobremesas == 2:
-                print('____________________________________________')
-                print('\nVocê adicionou Torta Gelada Maracuja ao seu pedido\n')
-                print('____________________________________________')
-                conta.calculoValor(64.90, 'Torta gelada de Maracuja')
+        cardapioSobremesas()
+        operacoes.contaSobremesa(this.opcaoSobremesa)
 
 
-            elif this.opcaoSobremesas == 3:
-                print('____________________________________________')
-                print('\nVocê adicionou Mousse ao seu pedido\n')
-                print('____________________________________________')
-                conta.calculoValor(59.00, "Mousse")
-
-            elif this.opcaoSobremesas == 4:
-                print('____________________________________________')
-                print('\nVocê adicionou Cheesecake Gelado ao seu pedido\n')
-                print('____________________________________________')
-                conta.calculoValor(69.00, 'Chessecake Gelado')
-
-            elif this.opcaoSobremesas == 5:
-                print('____________________________________________')
-                print('\nVocê adicionou Sobremesa de Amendoim ao seu pedido\n')
-                print('____________________________________________')
-                conta.calculoValor(52.00, 'Sobremesa de Amendoim')
-
-            elif this.opcaoSobremesas == 6:
-                cardapio.redirecionarCardapio()
-
-            else:
-                print('____________________________________________')
-                print('\nSelecione uma opção válida\n')
-                print('____________________________________________')
     except:
         print('____________________________________________')
         print('\nSelecione uma opção válida\n')
